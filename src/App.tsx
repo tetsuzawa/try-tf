dda61400c265749052b5ba3328fa4e64b7b990d3
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-10-15 23:57:37 +0900

    Fix theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,63 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {useState} from "react";
 import "./App.css";
+// @ts-ignore
+import CircularSlider from "@fseehawer/react-circular-slider";
+
+import Button from '@material-ui/core/Button';
+import AudiotrackIcon from '@material-ui/icons/Audiotrack';
+import {MuiThemeProvider} from "@material-ui/core";
+import {theme} from "./theme";
 
 function App() {
+
+  // const [audioSrc, setAudioSrc] = useState("");
+
+  const onChangeAudioSrc = () => {
+    // setAudioSrc("");
+    console.log("aaa")
+    const obj = document.getElementsByTagName("input")[0].files || "";
+    // const audio = document.getElementById("audio-src") as HTMLAudioElement;
+    const audio = document.getElementById("audio-src");
+    audio.srcObject = obj;
+    console.log(obj)
+    console.log(audio)
+  };
+  console.log("aaa")
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MuiThemeProvider theme={theme}>
+          <CircularSlider
+            min={0}
+            max={360}
+            direction={1}
+            knobPosition="top"
+            appendToValue="°"
+            valueFontSize="4rem"
+            labelColor="#61DAFB"
+            knobColor="#61DAFB"
+            progressColorFrom="#FFFFFF"
+            progressColorTo="#FFFFFF"
+            trackColor="#FFFFFF"
+            onChange={(value: any) => console.log(value)}
+          />
+          <label htmlFor="base-audio">
+            <input
+              type="file"
+              style={{display: 'none'}}
+              id="base-audio"
+              name="base-audio"
+              accept="audio/mpeg,audio/wav,audio/aiff"
+              onChange={onChangeAudioSrc}
+            />
+            <Button color="primary" variant="contained" component="span">
+              <AudiotrackIcon/>
+              Upload audio
+            </Button>
+          </label>
+          <audio controls id="audio-src" onChange={onChangeAudioSrc}/>
+        </MuiThemeProvider>
       </header>
     </div>
   );
